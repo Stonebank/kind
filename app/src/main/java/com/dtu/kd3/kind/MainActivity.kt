6a097a9fdbe6b47058f9c7bae9f1298bee53a6e4
Hassan K <hassan_99@live.dk>
2022-10-27 09:00:59 +0200

    Minor change

--- app/src/main/java/com/dtu/kd3/kind/MainActivity.kt
+++ app/src/main/java/com/dtu/kd3/kind/MainActivity.kt
@@ -1,33 +1,24 @@
 package com.dtu.kd3.kind
 
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import com.dtu.kd3.kind.controller.Navigation
 import com.dtu.kd3.kind.ui.theme.KindTheme
-import com.dtu.kd3.kind.views.LoginView
-import com.dtu.kd3.kind.views.ShowRegisterView
 
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         setContent {
             KindTheme {
                 Surface {
-                    ShowRegisterView()
+                    Navigation()
                 }
             }
         }
-    }
-
-    @Composable
-    fun ShowLoginPage() {
-        LoginView()
     }
 
 }
